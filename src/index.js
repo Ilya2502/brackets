971b7a121de10523c491a35bbab6f957f267aaf9
Ilya2502 <gavran2502@mail.ru>
2022-01-27 00:04:06 +0300

    feat: add code js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let value = 'look';
+  const result = str.split('');
+  let sum = result.length;
+  while (sum > 0) {
+    for (let i = 0; i < result.length; i++) {
+      for (let arr of bracketsConfig) {
+        if (arr.includes(result[i]) && result[i] === arr[0]) {
+          if (result[i + 1] === arr[1]) {
+            result.splice(i, 2);
+            value = 'find';
+            sum -= 1;
+            break;
+          } else {
+            value = 'look';
+            break;
+          }
+        } else if (arr.includes(result[i]) && result[i] !== arr[0]) {
+        return false;
+        }
+     }
+      if (value === 'find') {
+          break;
+        }
+    }
+    sum -= 1;
+  }
+  return result.length === 0 ? true : false;
 }

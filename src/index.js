a24e5a8d5e295bc83f56084339ff9f588d78df40
Ilya2502 <gavran2502@mail.ru>
2022-01-27 16:03:21 +0300

    refactor: delete while

--- src/index.js
+++ src/index.js
@@ -1,29 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  let value = 'look';
-  const result = str.split('');
+  let value = "look";
+  const result = str.split("");
   let sum = result.length;
-  while (sum > 0) {
     for (let i = 0; i < result.length; i++) {
       for (let arr of bracketsConfig) {
         if (arr.includes(result[i]) && result[i] === arr[0]) {
           if (result[i + 1] === arr[1]) {
             result.splice(i, 2);
-            value = 'find';
-            sum -= 1;
+            value = "find";
             break;
           } else {
-            value = 'look';
+            value = "look";
             break;
           }
         } else if (arr.includes(result[i]) && result[i] !== arr[0]) {
-        return false;
+          return false;
         }
-     }
-      if (value === 'find') {
-          break;
-        }
+      }
+      if (value === "find") {
+        i = -1;
+        continue;
+      }
     }
-    sum -= 1;
-  }
   return result.length === 0 ? true : false;
 }
